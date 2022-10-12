01f033826178b124259cbc52706749610712ee42
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-10-12 21:53:53 +0300

    Added function chang color background

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', startСhangColor);
+//stopBtn.addEventListener('click', stopСhangColor);
+
+function startСhangColor() {
+  setInterval(() => {
+    const colorBody = getRandomHexColor();
+    document.body.style.backgroundColor = colorBody;
+  }, 1000);
+}
