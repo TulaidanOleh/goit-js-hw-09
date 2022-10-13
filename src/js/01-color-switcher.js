ee7407e03252bbc56b1ebbc698398206f59ef9b8
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-10-13 16:18:30 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,27 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', startСhangColor);
+stopBtn.addEventListener('click', stopСhangColor);
+
+let timerId = null;
+
+stopBtn.disabled = true;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startBtn.addEventListener('click', startСhangColor);
-//stopBtn.addEventListener('click', stopСhangColor);
-
 function startСhangColor() {
-  setInterval(() => {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
     const colorBody = getRandomHexColor();
     document.body.style.backgroundColor = colorBody;
   }, 1000);
+  stopBtn.disabled = false;
+}
+
+function stopСhangColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 }
