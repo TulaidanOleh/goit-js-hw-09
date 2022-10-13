5813b6357f25d77f4cb41dcce007230d723168cc
TulaidanOleh <95975989+TulaidanOleh@users.noreply.github.com>
2022-10-13 13:18:58 +0000

    Deploying to gh-pages from @ TulaidanOleh/goit-js-hw-09@ee7407e03252bbc56b1ebbc698398206f59ef9b8 🚀

--- 01-color-switcher.272a1d54.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)}));
-//# sourceMappingURL=01-color-switcher.272a1d54.js.map
